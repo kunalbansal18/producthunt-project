ae4a2d2dfa4f371b82ac85fc12247f930ce7cb3e
Kunal Bansal <bansal18@iitg.ac.in>
2020-08-01 00:54:12 +0530

    final

--- producthunt/urls.py
+++ producthunt/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from productsapp import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.homepage, name = 'home'),
     path('accounts/',include('accountsapp.urls')),
-]
+    path('products/',include('productsapp.urls')),
+    ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
